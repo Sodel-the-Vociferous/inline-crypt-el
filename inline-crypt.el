7524700f7147f811b463bb0dfa0ad7572a814e5b
Daniel Ralston <daniel@dralston.net>
2017-08-24 01:57:45 -0700

    Update inline-crypt.el

--- inline-crypt.el
+++ inline-crypt.el
@@ -4,6 +4,19 @@
 ;;; Url: https://github.com/Sodel-the-Vociferous/inline-crypt-el
 ;;; Keywords: crypt
 
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; This package may be useful if you’d just like to encrypt parts of
